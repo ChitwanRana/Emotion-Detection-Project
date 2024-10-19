036af9f2e05d28326bdfe9829e80ae081f7d3362
ChitwanRana <kushr4446@gmail.com>
2024-10-20 01:17:17 +0530

    Model Training

---
+++ Test_Emotion.py
@@ -0,0 +1,19 @@
+import numpy
+import cv2
+from tensorflow.keras.models import model_from_json
+
+
+emotion_dict={0:'Angry',1:'Disgusted',2:'Fear',3:'Happy',4:'Neutral',5:'Sad',6:'Surprised'}
+
+
+#load json and create model
+json_file=open('model/emotion_model.json','r')
+loaded_model_json=json_file.read()
+json_file.close()
+emotion_model=model_from_json(loaded_model_json)
+
+#load weights into new model 
+emotion_model.load_weights("model/emotion_model.h5")
+print("loaded model from disk")
+
+
